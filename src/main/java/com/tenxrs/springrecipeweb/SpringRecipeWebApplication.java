65996077ff73a245f29c7d29bc1e6c672ca12dee
vmpenyana <vincent@tenxrs.com>
2023-08-25 20:06:47 +0200

    project initialization

---
+++ src/main/java/com/tenxrs/springrecipeweb/SpringRecipeWebApplication.java
@@ -0,0 +1,13 @@
+package com.tenxrs.springrecipeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenxrs/springrecipeweb/SpringRecipeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenxrs.springrecipeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
